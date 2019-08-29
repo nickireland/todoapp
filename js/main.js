3ea5797cbe73b774f356155cb898687a97a94685
Nick Ireland <nick.a.ireland@gmail.com>
2019-08-28 22:27:41 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//user clicked on add button
+//instead of breaking out the function we're keeping it inline because we're not reusing the code
+document.getElementById('add').addEventListener('click', function() {
+
+    var value = document.getElementById('item').value;
+    
+    if(value) {
+        
+    };
+
+});
